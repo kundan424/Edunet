a5ea90d5d6bc957b0d6509c49f592ef712c4124a
kundan kumar <kundankumar64355@gmail.com>
2026-09-14 10:39:48 +0530

    header fix

--- src/layouts/components/Shared.tsx
+++ src/layouts/components/Shared.tsx
@@ -70,10 +70,10 @@ export function Header() {
   );
 
   return (
-    <div className="pt-4 md:pt-6 px-4 sm:px-6 md:px-8 w-full max-w-full mx-auto top-0 z-50 ">
+    <div className="px-4 sm:px-6 md:px-8 w-full max-w-full mx-auto top-0 sticky z-50 ">
       <header className="bg-cream-paper/80 backdrop-blur-md rounded-2xl  flex items-center justify-between gap-6 px-4 md:px-6 py-3 relative">
         {/* Logo / Brand */}
-        <div className="flex items-center gap-6 shrink-0">
+        <div className="flex items-center gap-6 shrink-0 ">
           <Link to="/" className="font-degular-display text-2xl md:text-3xl leading-none font-bold tracking-heading-sm text-midnight-ink z-60">
             EduNet
           </Link>
